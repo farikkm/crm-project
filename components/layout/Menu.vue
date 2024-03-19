42ac0509f43c6a19b8a7e397fe81754c7b3c288a
farikkm <farikk.v.2@gmail.com>
2024-03-19 21:43:45 +0500

    Commited by another device

--- components/layout/Menu.vue
+++ components/layout/Menu.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { MENU_DATA } from './menu.data';
-
-
 </script>
 
 <template>
   <div>
+    <!-- text-xl for 2K Monitors -->
     <NuxtLink 
-      class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-3"
+      class="flex items-center text-xl py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-3"
       v-for="item in MENU_DATA" 
       :key="item.name"
       :to="item.url"
