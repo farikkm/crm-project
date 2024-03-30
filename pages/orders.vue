da9fad3463f559656a85bcb1f119ffcebd5a9f5f
farikkm <farikk.v.2@gmail.com>
2024-03-30 13:35:08 +0500

    All pages are created

---
+++ pages/orders.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Orders page
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Products page
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
